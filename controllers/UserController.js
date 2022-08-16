5c23cac1c14748ea4bf04d9c682405c541f1effc
KawsarAlHasan <kawsaralhasan.420@gmail.com>
2022-08-17 02:06:19 +0600

    added user delete controller

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -42,3 +42,21 @@ export const updateUser = async (req, res) => {
     res.status(403).json('Access Denied! You can only update your own profile')
   }
 }
+
+// Delete a User
+export const deleteUser = async (req, res) => {
+  const id = req.params.id
+
+  const { currentUserId, currentUserAdminstatus } = req.body
+
+  if (currentUserId === id || currentUserAdminstatus) {
+    try {
+      await UserModel.findByIdAndDelete(id)
+      res.status(200).json('User deleted successfully')
+    } catch (error) {
+      res.status(500).json(error)
+    }
+  } else {
+    res.status(403).json('Access Denied! You can only delete your own profile')
+  }
+}
--- routes/UserRoute.js
+++ routes/UserRoute.js
@@ -1,9 +1,14 @@
 import express from 'express'
-import { getUser, updateUser } from '../controllers/UserController.js'
+import {
+  deleteUser,
+  getUser,
+  updateUser,
+} from '../controllers/UserController.js'
 
 const router = express.Router()
 
 router.get('/:id', getUser)
 router.put('/:id', updateUser)
+router.delete('/:id', deleteUser)
 
 export default router
