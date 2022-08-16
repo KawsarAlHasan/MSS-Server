6d41ab976c4dfd90cc53ae6a12ef6da2ef68f228
KawsarAlHasan <kawsaralhasan.420@gmail.com>
2022-08-16 22:47:07 +0600

    added auth controller Login

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -22,3 +22,22 @@ export const registerUser = async (req, res) => {
     res.status(500).json({ message: error.message })
   }
 }
+
+// login user
+export const loginUser = async (req, res) => {
+  const { username, password } = req.body
+  try {
+    const user = await UserModel.findOne({ username: username })
+    if (user) {
+      const validity = await bcrypt.compare(password, user.password)
+
+      validity
+        ? res.status(200).json(user)
+        : res.status(400).json('Wrong Password')
+    } else {
+      res.status(404).json('User does exists')
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+  }
+}
