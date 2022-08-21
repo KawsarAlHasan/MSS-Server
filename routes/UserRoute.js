58ff1d3a7e19e8f5d9bcaeee260140cc796ea181
KawsarAlHasan <kawsaralhasan.420@gmail.com>
2022-08-21 22:38:20 +0600

    added auth middleware

--- routes/UserRoute.js
+++ routes/UserRoute.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import authMiddleWare from '../middleware/AuthMiddleware.js'
 import {
   deleteUser,
   followUser,
@@ -12,9 +13,9 @@ const router = express.Router()
 
 router.get('/', getAllUsers)
 router.get('/:id', getUser)
-router.put('/:id', updateUser)
-router.delete('/:id', deleteUser)
-router.put('/:id/follow', followUser)
-router.put('/:id/unfollow', unFollowUser)
+router.put('/:id', authMiddleWare, updateUser)
+router.delete('/:id', authMiddleWare, deleteUser)
+router.put('/:id/follow', authMiddleWare, followUser)
+router.put('/:id/unfollow', authMiddleWare, unFollowUser)
 
 export default router
