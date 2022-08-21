13373a4dee7a9ccb988bf24e2f6de394700def5b
Nahid Ahmmed Likhon <lemonnahid19@gmail>
2022-08-21 17:18:22 +0600

    new

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use("/images", express.static("images"));
 // Middleware
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
+app.use(cors({ origin: "https://powerful-plains-57380.herokuapp.com" }));
 dotenv.config();
 
 mongoose
